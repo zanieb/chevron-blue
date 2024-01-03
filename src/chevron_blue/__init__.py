cdfb13863da461bb65b3a190c6a0113e1e60d9cb
Zanie <contact@zanie.dev>
2024-01-03 12:33:07 -0600

    Run ruff formatter

--- src/chevron_blue/__init__.py
+++ src/chevron_blue/__init__.py
@@ -2,4 +2,4 @@ from .main import cli_main, main
 from .renderer import render
 from .tokenizer import ChevronError
 
-__all__ = ['main', 'render', 'cli_main', 'ChevronError']
+__all__ = ["main", "render", "cli_main", "ChevronError"]
